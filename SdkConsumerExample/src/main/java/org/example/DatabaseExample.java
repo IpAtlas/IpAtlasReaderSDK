137a3e7c2aa934f24be8caaad1cd0748924d16e3
Milan Adamovic <madamovic.bg@gmail.com>
2024-03-03 12:46:56 +0100

    Update SDK example

--- SdkConsumerExample/src/main/java/org/example/DatabaseExample.java
+++ SdkConsumerExample/src/main/java/org/example/DatabaseExample.java
@@ -12,6 +12,9 @@ import java.util.Optional;
 public class DatabaseExample {
     private static final String DATABASE_PATH = "path/to/database/dynamic-plus-db-20240302.ipatlas";
 
+    /**
+     * Load and consume the database from the file system.
+     */
     public void LoadAndConsumeDatabase() {
         DatabaseReader databaseReader = new DatabaseReaderBuilder()
                 .databasePath(DATABASE_PATH)
@@ -28,6 +31,9 @@ public class DatabaseExample {
         }
     }
 
+    /**
+     * Load and consume the database from the input stream.
+     */
     public void LoadFromInputStreamAndConsumeDatabase() throws FileNotFoundException {
         InputStream inputStream = new FileInputStream(DATABASE_PATH);
         DatabaseReader databaseReader = new DatabaseReaderBuilder()
@@ -45,9 +51,13 @@ public class DatabaseExample {
         }
     }
 
+    /**
+     * Load and consume the database from the license key. This method requires an internet connection to download the database.
+     */
     public void LoadFromKeyAndConsumeDatabase() throws FileNotFoundException {
         DatabaseReader databaseReader = new DatabaseReaderBuilder()
-                .databaseKey("your-database-key")
+                .databaseLicenseKey("your-database-license-key")
+                .databaseDownloadLocation("path/to/download/database")
                 .build();
 
         Optional<GeolocationData> optional = databaseReader.resolveIp("101.98.226.225");
